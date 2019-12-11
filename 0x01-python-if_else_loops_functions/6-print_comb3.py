92773a1201d085c5d162731d73be41002ac3c352
ariana124 <ariana.bibiano@gmail.com>
2019-12-11 05:44:15 +0000

    removed last comma and added newline

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,4 +2,8 @@
 
 for num1 in range(0, 10):
     for num2 in range(num1 + 1, 10):
-        print("{:d}{:d}".format(num1, num2), end=", ")
+        print("{:d}{:d}".format(num1, num2), end='')
+        if num1 != 8:
+            print(", ", end='')
+        else:
+            print("\n", end='')
