b3e020182b54c352cdc26339b1aebd1912a481e3
ariana124 <ariana.bibiano@gmail.com>
2019-12-12 22:40:03 +0000

    changed No argument(s). to 0 arguments.

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,7 +4,7 @@ import sys
 if __name__ == "__main__":
     count = len(sys.argv)
     if count == 1:
-        print("No argument(s).")
+        print("0 arguments.")
     elif count == 2:
         print("1 argument: ")
     else:
