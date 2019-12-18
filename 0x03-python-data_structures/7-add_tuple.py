9dfa280914f17c3d8e730de8397dec3eae757a7e
ariana124 <ariana.bibiano@gmail.com>
2019-12-18 01:33:57 +0000

    first commit

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    if len(tuple_a) < 1:
+        a1 = 0
+    else:
+        a1 = tuple_a[0]
+
+    if len(tuple_b) < 1:
+        b1 = 0
+    else:
+        b1 = tuple_b[0]
+
+    if len(tuple_a) < 2:
+        a2 = 0
+    else:
+        a2 = tuple_a[1]
+
+    if len(tuple_b) < 2:
+        b2 = 0
+    else:
+        b2 = tuple_b[1]
+
+    sum1 = a1 + b1
+    sum2 = a2 + b2
+    return (sum1, sum2)
