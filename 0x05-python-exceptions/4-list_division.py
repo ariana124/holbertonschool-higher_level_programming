853f3c2aac07986a8fe3475ca051cd693ba462a0
ariana124 <ariana.bibiano@gmail.com>
2020-01-09 00:21:42 +0000

    changed zero to 0

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -8,7 +8,7 @@ def list_division(my_list_1, my_list_2, list_length):
             result = my_list_1[i] / my_list_2[i]
         except ZeroDivisionError:
             result = 0
-            print("division by zero")
+            print("division by 0")
         except IndexError:
             result = 0
             print("out of range")
