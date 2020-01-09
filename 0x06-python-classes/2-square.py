d42a6c5958619aae64437be592c111dab1104da8
ariana124 <ariana.bibiano@gmail.com>
2020-01-09 22:21:33 +0000

    fixed pep8 spacing

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -3,7 +3,7 @@
 
 class Square:
 
-    def __init__(self, size = 0):
+    def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
