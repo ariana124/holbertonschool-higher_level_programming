e704a545a5503a4c6442c33f41e9fd44f3ba91a1
ariana124 <ariana.bibiano@gmail.com>
2020-01-13 20:05:03 +0000

    first commit

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+def print_square(size):
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    if isinstance(size, float) and size < 0:
+        raise TypeError("size must be an integer")
+    for y in range(size):
+        for x in range(size):
+            print("#", end='')
+        print()
