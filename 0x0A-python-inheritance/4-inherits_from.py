ae6b1918137804fa3eab8b7d03523f3595e0c3ec
ariana124 <ariana.bibiano@gmail.com>
2020-01-21 19:19:19 +0000

    removed isinstance and switched with type()

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -7,4 +7,4 @@ Module that contains the function inherits_from
 def inherits_from(obj, a_class):
     """ returns True if the object is an instance of a class that inherited
     (directly or indirectly) from the specified class, otherwise False """
-    return isinstance(type(obj), a_class) is not issubclass(type(obj), a_class)
+    return issubclass(type(obj), a_class) and type(obj) is not a_class
