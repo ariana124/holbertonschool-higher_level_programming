b0f9c689aa33dd0bdb4e69e05affafb1c4fd4cee
ariana124 <ariana.bibiano@gmail.com>
2020-01-21 01:24:48 +0000

    first commit

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Module containing the class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    def area(self):
+        """ public instance area method """
+        raise Exception("area() is not implemented")
