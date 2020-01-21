0ff7d4577d516fd0afe77ccb1f70b6dbb7e9ca58
ariana124 <ariana.bibiano@gmail.com>
2020-01-21 02:41:59 +0000

    first commit

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Module containing the class Rectangle with properties from BaseGeometry
+"""
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    def __init__(self, size):
+        """ initializes class Square """
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """ public instance area method """
+        return self.__size ** 2
+
+    def __str__(self):
+        """ prints square description """
+        return "[Square] {}/{}".format(self.__size, self.__size)
