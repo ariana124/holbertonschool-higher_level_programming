e1fbed530361887e40105029784e1f5ef9aba97c
ariana124 <ariana.bibiano@gmail.com>
2020-01-21 01:38:41 +0000

    first commit

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Module containing the class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    def integer_validator(self, name, value):
+        """ public instance method that validates value """
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+    def area(self):
+        """ public instance area method """
+        raise Exception("area() is not implemented")
