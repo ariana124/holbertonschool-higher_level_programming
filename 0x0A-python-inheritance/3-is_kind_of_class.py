cff00b3bcb382624e74b2e35641492af3563ce7a
ariana124 <ariana.bibiano@gmail.com>
2020-01-21 00:42:12 +0000

    removed extra space in function return description

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -7,5 +7,5 @@ Module that contains the function is_kind_of_class
 def is_kind_of_class(obj, a_class):
     """ returns True if the object is an instance of, or if the object is an
     instance of a class that inherited from, the specified class, otherwise
-    it returns False  """
+    it returns False """
     return isinstance(obj, a_class)
