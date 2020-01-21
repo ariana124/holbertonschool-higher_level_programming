cc4a990e5d40d6b458e869c19d1f83777333e5e4
ariana124 <ariana.bibiano@gmail.com>
2020-01-21 02:29:28 +0000

    added documentation for initializing Rectangle

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -7,6 +7,7 @@ BaseGeometry = __import__("7-base_geometry").BaseGeometry
 
 class Rectangle(BaseGeometry):
     def __init__(self, width, height):
+        """ initializes class Rectangle """
         self.integer_validator("width", width)
         self.__width = width
         self.integer_validator("height", height)
