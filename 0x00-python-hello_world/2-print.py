8e35ce37c2580b6ed5e9af5d93d74c010932fc29
ariana124 <ariana.bibiano@gmail.com>
2020-02-09 06:13:35 +0000

    added documentation

--- 0x00-python-hello_world/2-print.py
+++ 0x00-python-hello_world/2-print.py
@@ -1,2 +1,7 @@
 #!/usr/bin/python3
+"""
+ Python script that prints exactly "Programming is like building a multilingual
+ puzzle, followed by a new line
+"""
+
 print("\"Programming is like building a multilingual puzzle")
