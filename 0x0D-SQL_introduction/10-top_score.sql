fcb94587b7d3b5453920aacc867401ea617baace
ariana124 <ariana.bibiano@gmail.com>
2020-03-03 01:18:05 +0000

    prints scores in order

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,5 @@
+-- Lists all records of the table 'second_table'
+--    + displays the score and the name
+--    + should be ordered from score
+
+SELECT score, name FROM second_table ORDER BY score DESC;
