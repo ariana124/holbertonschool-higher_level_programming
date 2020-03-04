cf059d4a7c23f6eaa3929e71da8f5e6b3d55e61b
ariana124 <ariana.bibiano@gmail.com>
2020-03-04 21:41:20 +0000

    lists all shows

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,8 @@
+-- Lists all the shows in the database 'hbtn_0d_tvshows' without a genre linked
+
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.show_id IS NULL
+ORDER by 1, 2 ASC;
