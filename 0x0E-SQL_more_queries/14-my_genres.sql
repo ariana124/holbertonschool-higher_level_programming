a8066da7d5e691677c3783ab9c88f5a614653c5a
ariana124 <ariana.bibiano@gmail.com>
2020-03-04 22:08:27 +0000

    lists genres of a show

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,8 @@
+-- Lists all the genres of the show Dexter
+
+SELECT tv_genres.name AS name
+FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+WHERE title = 'Dexter'
+ORDER by tv_genres.name ASC;
