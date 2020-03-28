68cc9501223dd613f0733ebf53bb3a8cf71065a3
ariana124 <ariana.bibiano@gmail.com>
2020-03-28 22:09:40 +0000

    filters states by user input

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Contains the script that lists all states starting with a capital N
+from the database hbtn_0e_0_usa
+"""
+
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    host = "localhost"
+    port = 3306
+    user = argv[1]
+    passwd = argv[2]
+    database = argv[3]
+    sname = argv[4]
+
+    db = MySQLdb.connect(host=host, port=port, user=user,
+                         passwd=passwd, db=database)
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM states WHERE name = '{}' \
+    ORDER BY id;".format(sname))
+
+    rows = cur.fetchall()
+    for state in rows:
+        print(state)
+
+    cur.close()
+    db.close()
