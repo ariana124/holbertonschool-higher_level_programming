76ebc1121f38e963b787d8f50053f9734dca3a87
ariana124 <ariana.bibiano@gmail.com>
2020-04-13 20:27:28 +0000

    gets email

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+script that takes in a URL and an email, sends a POST request to the passed URL
+"""
+import urllib.request
+from sys import argv
+
+
+if __name__ == "__main__":
+    values = {'email': argv[2]}
+    data = urllib.parse.urlencode(values)
+    data = data.encode('ascii')
+    req = urllib.request.Request(argv[1], data)
+    with urllib.request.urlopen(req) as response:
+        html = response.read()
+        print(html.decode('utf-8'))
