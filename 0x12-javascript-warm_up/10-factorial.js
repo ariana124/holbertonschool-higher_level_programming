ac62890dbcf53b357b4c9b32747c4e8ffbe3cdb5
ariana124 <ariana.bibiano@gmail.com>
2020-05-05 22:00:38 +0000

    prints factorial of a number

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+// Computes and prints a factorial
+function factorial (n) {
+ if (isNaN(n)) {
+  return (1);
+ } else if (n === 1) {
+   return (1);
+ } else {
+   return (n * factorial(n - 1));
+ }
+}
+
+const n = parseInt(process.argv[2]);
+console.log(factorial(n));
