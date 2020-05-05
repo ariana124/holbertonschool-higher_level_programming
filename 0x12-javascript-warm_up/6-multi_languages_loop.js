4f05dd1d90e0a850e4e5f720a660428dcc6c8b91
ariana124 <ariana.bibiano@gmail.com>
2020-05-05 20:17:23 +0000

    first loop through array

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+// Prints values in an array
+const languages = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (let i = 0; i < languages.length; i++) {
+  console.log(languages[i]);
+}
