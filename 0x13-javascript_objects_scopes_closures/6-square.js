d130fd15a112bcd64c97e86ac084cdd450c975e5
ariana124 <ariana.bibiano@gmail.com>
2020-05-11 23:10:01 +0000

    prints square if it's undefined or not

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+// Creates the Square class and prints it using the letter c
+
+const square = require('./5-square');
+module.exports = class Square extends square {
+  charPrint(c) {
+    if (c == undefined) {
+       this.print();
+    } else {
+      for (let i = 0; i < this.width; i++) {
+	console.log('C'.repeat(this.width));
+      }
+    }
+  }
+};
