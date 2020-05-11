b4b3fcaf1590c1530b21685ac33e7ef64bfa3a9e
ariana124 <ariana.bibiano@gmail.com>
2020-05-11 18:48:34 +0000

    creates a rectangle with height and width

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// Creates a class that defines a Rectangle
+
+class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+
+module.exports = Rectangle;
