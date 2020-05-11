f20a6bcb8f484635d6bcc39a98daa2a3de5588c7
ariana124 <ariana.bibiano@gmail.com>
2020-05-11 23:32:23 +0000

    number conversion

---
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// Function that converts a number from base 10 to another base
+
+exports.converter = function (base) {
+  return function (number) {
+    return number.toString(base);
+  };
+};
