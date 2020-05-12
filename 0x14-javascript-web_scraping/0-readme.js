aba04e1f8f3ba3d72a138eeccf815d3d8d218e6f
ariana124 <ariana.bibiano@gmail.com>
2020-05-12 23:06:13 +0000

    reads and prints the contents of a file

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// Script that reads and prints the contents of a file
+
+const fs = require('fs');
+const file = process.argv[2];
+fs.readFile(file, 'utf-8', function (err, data) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+});
