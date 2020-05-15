fd3b21ef87d1303184b0f9de8d9dad22767715df
ariana124 <ariana.bibiano@gmail.com>
2020-05-15 01:42:10 +0000

    changed to correct header

--- 0x15-javascript-web_jquery/2-script.js
+++ 0x15-javascript-web_jquery/2-script.js
@@ -1,4 +1,4 @@
 // Script that adds the class red to the header tag and changes it to the color red on click
 $('#red_header').click(function () {
-  $('header').css('color', '#FF0000');
+  $('#red_header').css('color', '#FF0000');
 });
